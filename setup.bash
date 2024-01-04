762e3a9e671b6fd60683ee8ae03f3cf8ca212ee1
Yamashita <c3260@st.cs.kumamoto-u.ac.jp>
2024-01-04 13:12:10 +0900

    make swapfile

--- setup.bash
+++ setup.bash
@@ -4,6 +4,12 @@ sudo apt update
 
 sudo apt upgrade
 
+sudo fallocate -l 1g /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
+sudo swapon -a
+
 sudo apt install xubuntu-desktop
 
 sudo reboot
